68b918439f1376798021950eb22d428378218449
Marvin Kurland <bugemarvin@outlook.com>
2023-01-06 02:01:29 +0300

    commists

---
+++ staticfiles/js/myjs.js
@@ -0,0 +1,8 @@
+const $ = window.$;
+$(document).ready(function () {
+  $('[data-bg-img').each(function () {
+    $(this).css({
+      'background-image': `url(${$(this).data('bg-img')})`
+    });
+  });
+});
