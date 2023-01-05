b29b1d06a33eb00c530211598bb6e3471ef907d1
Marvin Kurland <bugemarvin@outlook.com>
2023-01-05 21:09:30 +0300

    adding images and jquery

---
+++ static/js/jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+        $('[bg-img]').each(function(){
+                $(this).css({
+                        'background: url(/static/images/bg/alex-holt-wpykOchTDv4-unsplash.jpg);'
+                })
+        })
+})
