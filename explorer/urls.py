cbe0d08d79608340fb3fd7c2555453327997dff4
Marvin Kurland <bugemarvin@outlook.com>
2023-01-11 23:41:22 +0300

    refactoring code

--- explorer/urls.py
+++ explorer/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path("dashboard/", views.dashboard, name="index"),
+    path("maps/", views.maps, name="index"),
+    path("messages/", views.messages, name="index"),
+    path("notification/", views.notification, name="index"),
+    path("settings/", views.settings, name="index"),
 ]
--- explorer/views.py
+++ explorer/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 def index(request):
         return render(request, 'explorer/index.html', {})
 
+def dashboard(request):
+        return render(request, 'dashboard/index.html', {})
+
+def maps(request):
+        return render(request, 'dashboard/maps.html', {})
+
+def messages(request):
+        return render(request, 'dashboard/messages.html', {})
+
+def notification(request):
+        return render(request, 'dashboard/notification.html', {})
+
+def settings(request):
+        return render(request, 'dashboard/settings.html', {})
