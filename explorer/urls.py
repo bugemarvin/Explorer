4c2643add66f6039ec7a690c6a087d70c5169a0a
Marvin Kurland <bugemarvin@outlook.com>
2023-01-12 21:34:53 +0300

    adding user authentications ui's

--- explorer/urls.py
+++ explorer/urls.py
@@ -4,8 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path("dashboard/", views.dashboard, name="index"),
-    path("maps/", views.maps, name="index"),
-    path("messages/", views.messages, name="index"),
-    path("notification/", views.notification, name="index"),
-    path("settings/", views.settings, name="index"),
-]
+    path("maps/", views.maps, name="maps"),
+    path("messages/", views.messages, name="messages"),
+    path("notification/", views.notification, name="notification"),
+    path("settings/", views.settings, name="settings"),
+    path("login/", views.login, name="index"),
+    path("signup/", views.signup, name="register"),
+    path("reset/", views.resets, name="reset"),
+    path("forgot/", views.forgot, name="forgot"),
+] 
--- explorer/views.py
+++ explorer/views.py
@@ -17,3 +17,15 @@ def notification(request):
 
 def settings(request):
         return render(request, 'dashboard/settings.html', {})
+
+def login(request):
+        return render(request, 'authentication/index.html', {})
+
+def signup(request):
+        return render(request, 'authentication/register.html', {})
+
+def resets(request):
+        return render(request, 'authentication/reset.html', {})
+
+def forgot(request):
+        return render(request, 'authentication/forgot.html', {})
