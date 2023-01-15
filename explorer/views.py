462492d28d9806680b1bb38143363e0506193841
Marvin Kurland <bugemarvin@outlook.com>
2023-01-15 16:48:47 +0300

    registration creation

--- explorer/views.py
+++ explorer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import 
 from django.contrib import messages
 
 def index(request):
